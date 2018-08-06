e2a67a304a8e890de10e6cb8c03ee6898fb61bf9
Victor M. Reyes <oicrruf.0286@gmail.com>
2018-08-05 19:52:06 -0600

    Instalación de WebPack-Encore

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('web/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+
+    .addEntry('app', './assets/js/app.js')
+
+;
+
+module.exports = Encore.getWebpackConfig();
